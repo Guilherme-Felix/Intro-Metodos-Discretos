d3172bf38db3b4205aa0ebcfe8067f079a798318
Guilherme-Felix <guinga4z@gmail.com>
2018-04-17 15:17:54 -0300

    Livros, foto do quadro e exercicio 1

---
+++ Exercicio1_EulerExplicito.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N1 = 30 
+N2 = 135
+h1 = 1./N1
+h2 = 1./N2
+
+x1 = np.arange(0,1,h1)
+x2 = np.arange(0,1,h2)
+
+y1 = np.zeros(N1)
+y2 = np.zeros(N2)
+
+def f(x):
+    return np.arctan(x)
+
+for i in range(N1-1):
+    y1[i+1] = y1[i] + h1*( f(y1[i]) )
+
+for i in range(N2-1):
+    y2[i+1] = y2[i] + h2*( f(y2[i]) )
+
+plt.plot(x1, y1, 'r.')
+plt.plot(x2, y2, 'bo')
+plt.grid()
+plt.show()
+
+
