5229c2f969ab92e7fb053f4193e5f45acb82bfe8
Guilherme-Felix <guinga4z@gmail.com>
2018-06-21 12:03:40 -0300

    Exercicio resolvido pela regra do trapezio

--- Exercicio1_Trapezio.py
+++ Exercicio1_Trapezio.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize 
 
+'''
+Implementacao do metodo do trapezio.
+'''
 
 N1 = 30 
 N2 = 135
@@ -17,12 +20,18 @@ y2 = np.zeros(N2)
 y1[0] = 1
 y2[0] = 1
 
+def f(Y):
+    return np.arctan(Y)
+
 def F(Y, h, yk):
-    return Y - h*np.arctan(Y) - yk
+    return Y - (h/2)*( f(Y) + f(yk) ) - yk
+
+def Fprime(Y, h, yk):
+    return 1 - (h/2)*( 1./(1 + Y**2) ) 
 
 # Metodo de Euler Implicito para 30 pontos
 for i in range(N1-1):
-    y1[i+1] = optimize.newton(F, y1[i], args=(h1, y1[i],))
+    y1[i+1] = optimize.newton(F, y1[i], fprime=Fprime, args=(h1, y1[i],))
 
 # Metodo de Euler Implicito para 135 pontos
 for i in range(N2-1):
@@ -31,10 +40,8 @@ for i in range(N2-1):
 # Plot 1
 plt.plot(x1, y1, 'r.', label="30 pontos",  linewidth=1)
 plt.plot(x2, y2, 'b:', label="135 pontos", linewidth=1)
-plt.title("Metodo de Euler Implicito")
+plt.title("Metodo do trapezio")
 plt.legend()
 plt.grid()
-plt.savefig("EulerImplicito.png")
+plt.savefig("Trapezio.png")
 plt.show()
-
-
